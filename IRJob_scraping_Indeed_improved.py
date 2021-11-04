1a7ed45a873f95e17602d98c0d8d6894d3fe0915
tdnguy16 <tdnguy16@gmail.com>
2021-11-04 23:14:32 +0700

    update

--- IRJob_scraping_Indeed_improved.py
+++ IRJob_scraping_Indeed_improved.py
@@ -14,51 +14,36 @@ def extract(page):
 def transform(soup):
     job_posts = soup.find_all('a', 'tapItem')
     for post in job_posts:
+        job_list = []
         # GET JOB TITLE
         job_title = post.h2('span')[-1].text
-        print(job_title)
-
+        job_list.append(job_title)
 
         # GET COMPANY NAME
         company_name = post.pre.find('span').text
-        print(company_name)
+        job_list.append(company_name)
 
         # GET COMPANY LOCATION
         company_location = post.find('div', 'companyLocation').text
-        print(company_location)
-
+        job_list.append(company_location)
 
         # GET LINK TO JOB DETAILS
-        links = soup.find_all('a', 'tapItem')
-        links_list = []
-        for link in links:
-            x = 'https://www.indeed.com' + link.get('href')
-            links_list.append(x)
-        # print(len(links_list))
-        # print(links_list)
+        job_link = 'https://www.indeed.com' + post.get('href')
+        job_list.append(job_link)
 
-    # COMBINE INFO INTO A LIST OF LISTS
-    # for i in range(len(job_list)):
-    #     listings = []
-    #     for key in job_list:
-    #         listings.append(key)
-    #         job_list.remove(key)
-    #         total_list.append(listings)
-    # print(len(listings))
-    # print(listings)
-    # return
+        # COMBINE INFO INTO A LIST OF LISTS
+        total_list.append(job_list)
 
 total_list = []
 
 # SCRAPE THROUGH PAGES
-for i in range(0,10,10):
+for i in range(0,30,10):
     c = extract(i)
     transform(c)
 
-# print(len(total_list))
-# print(total_list)
-#
-# data = pd.DataFrame(total_list, columns=('Job','Company','Location','Website'))
-# print(data)
-#
-# data.to_excel("IRjob.xlsx")
+# Make Dataframe from lists of jobs
+data = pd.DataFrame(total_list, columns=('Job','Company','Location','Website'))
+print(data)
+
+# Export dataframe to an excel file
+data.to_excel("IRjob.xlsx")
--- Job_scraping_Indeed.py
+++ Job_scraping_Indeed.py
@@ -7,12 +7,13 @@ import pandas
 def extract(page):
     url = f'https://www.indeed.com/jobs?q=institutional%20research&start={page}'
     r = requests.get(url)
-    soup = BeautifulSoup(r.content, 'html.parser')
+    soup = BeautifulSoup(r.text, 'html.parser')
     return soup
 
 
 def transform(soup):
-    job_title = soup.select('h2', class_='jobTitle')
+    # GET JOB TITLE
+    job_title = soup.find_all('h2', 'jobTitle')
     job_list = []
     for job in job_title:
         title = (job.find_all('span')[-1]).text
@@ -20,7 +21,8 @@ def transform(soup):
     # print(len(job_list))
     # print(job_list)
 
-    company_name = soup.select('pre')
+    # GET COMPANY NAME
+    company_name = soup.find_all('pre')
     company_list = []
     for company in company_name:
         name = (company.find('span')).text
@@ -28,7 +30,8 @@ def transform(soup):
     # print(len(company_list))
     # print(company_list)
 
-    location = soup.select('pre')
+    # GET COMPANY LOCATION
+    location = soup.find_all('pre')
     location_list = []
     for loc in location:
         city = (loc.find('div')).text
@@ -36,13 +39,16 @@ def transform(soup):
     # print(len(location_list))
     # print(location_list)
 
-    # listings = {}
-    # for key in job_list:
-    #     for value in company_list:
-    #         listings[key] = value
-    #         company_list.remove(value)
-    #         break
+    # GET LINK TO JOB DETAILS
+    links = soup.find_all('a', 'tapItem')
+    links_list = []
+    for link in links:
+        x = 'https://www.indeed.com' + link.get('href')
+        links_list.append(x)
+    # print(len(links_list))
+    # print(links_list)
 
+    # COMBINE INFO INTO A LIST OF LISTS
     for i in range(len(job_list)):
         listings = []
         for key in job_list:
@@ -54,7 +60,11 @@ def transform(soup):
                 for loc in location_list:
                     listings.append(loc)
                     location_list.remove(loc)
-                    total_list.append(listings)
+                    for link in links_list:
+                        listings.append(link)
+                        links_list.remove(link)
+                        total_list.append(listings)
+                        break
                     break
                 break
             break
@@ -64,12 +74,15 @@ def transform(soup):
 
 total_list = []
 
-for i in range(0,20,10):
+# SCRAPE THROUGH PAGES
+for i in range(0,100,10):
     c = extract(i)
     transform(c)
 
 print(len(total_list))
 print(total_list)
 
-data = pd.DataFrame(total_list)
+data = pd.DataFrame(total_list, columns=('Job','Company','Location','Website'))
 print(data)
+
+data.to_excel("IRjob.xlsx")
